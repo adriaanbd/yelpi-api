d1c2b6a53c9c0af24f2d51b102d0258d132302de
Adriaan Beiertz <adriaanbd@gmail.com>
2019-12-08 13:24:29 -0500

    update and delete unit tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { FactoryBot.build(:user) }
+  let(:saved_user) { FactoryBot.create(:user) }
 
   describe 'CRUD operations at unit level' do 
     context 'happy path' do
@@ -39,6 +40,37 @@ RSpec.describe User, type: :model do
       it 'is not valid without password' do 
         user.password_confirmation = 'test';
         expect(user).to_not be_valid
+      end
+    end
+
+    context 'update' do
+
+      before do 
+        saved_user.name = 'john'
+        saved_user.surname = 'doe'
+        saved_user.email = 'johndoe@example.com'
+        saved_user.password = 'test012'
+        saved_user.save!
+      end
+
+      it 'updates name' do
+        name = saved_user.name + ' ' + saved_user.surname
+        expect(name).to eq('john doe')
+      end
+
+      it 'updates email' do
+        expect(saved_user.email).to eq('johndoe@example.com')
+      end
+
+      it 'updates password' do
+        expect(saved_user.password).to eq('test012')
+      end
+    end
+
+    context 'delete' do
+      it 'deletes a record' do 
+        saved_user.destroy
+        expect { saved_user.reload }.to raise_error ActiveRecord::RecordNotFound
       end
     end
   end
