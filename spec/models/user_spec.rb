06d037434547526f2808bd5e16dbfb1612d5b782
Adriaan Beiertz <adriaanbd@gmail.com>
2019-12-08 10:27:02 -0500

    adding factory and happy path test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.build(:user) }
+
+  describe 'CRUD operations at unit level' do 
+    context 'happy path' do
+
+      before do
+        user.save!
+      end
+
+      it 'creates a user record with valid fields' do
+        p user.inspect()
+        expect(user.id).to be_truthy
+      end
+    end
+  end
 end
