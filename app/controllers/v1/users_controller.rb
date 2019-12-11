e025087b97896f46edc2d95f8226e6ed525cfb09
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-11 19:55:22 +0800

    Implement user authorization and update

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,11 +1,13 @@
 class V1::UsersController < ApplicationController
+  before_action :pundit_user, except: [:create]
+
   def show
   end
   
   def create
     user = User.new(user_params)
     if user.save
-      token = JsonWebToken.encode({id: user.id})
+      token = generate_token(user.id)
       render json: {user: user.attributes, token: token }
     else
       render json: { errors: user.errors, status: 422 }
@@ -13,6 +15,15 @@ class V1::UsersController < ApplicationController
   end
 
   def update
+    user = find_user
+    return unless user
+
+    authorize user
+    if user.update(user_params)
+      render json: {user: user.attributes, token: generate_token(user.id)}
+    else
+      render json: {error: "Cannot update account"}
+    end
   end
 
   def destroy
@@ -29,5 +40,17 @@ class V1::UsersController < ApplicationController
       :last_name,
       :profile_pic
     )
+  end
+
+  def find_user
+    user = User.find_by(id: params[:id])
+    return user if user
+
+    render json: { error: "Cannot find user" }
+    nil
+  end
+
+  def generate_token(id)
+    JsonWebToken.encode({id: id})
   end
 end
