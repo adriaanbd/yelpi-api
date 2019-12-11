e66fc4c17b9035c1738bbe0afeba5bf453948247
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-11 18:06:03 +0800

    Generate Users controller

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,14 @@
+class V1::UsersController < ApplicationController
+  def show
+  end
+  
+  def create
+    user = 
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :v1, defaults: { format: :json } do
+    resources :users, only: [:show, :create, :update, :destroy]
+  end
 end
