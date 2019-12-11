00111bd218eacfa51db947a3af1107f06dd3a0a4
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-11 18:24:16 +0800

    Create user create action

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -3,12 +3,31 @@ class V1::UsersController < ApplicationController
   end
   
   def create
-    user = 
+    user = User.new(user_params)
+    if user.save
+      token = JsonWebToken.encode({id: user.id})
+      render json: {user: user.attributes, token: token }
+    else
+      render json: { errors: user.errors, status: 422 }
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :password,
+      :password_confirmation,
+      :given_name,
+      :last_name,
+      :profile_pic
+    )
   end
 end
