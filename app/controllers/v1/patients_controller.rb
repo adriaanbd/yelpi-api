c1f0b846283c67019867066c30b32842e3ef5162
Adriaan Beiertz <adriaanbd@gmail.com>
2019-12-15 17:34:47 -0500

    implemented update and destroy actions

--- app/controllers/v1/patients_controller.rb
+++ app/controllers/v1/patients_controller.rb
@@ -1,10 +1,14 @@
 class V1::PatientsController < ApplicationController
   before_action :pundit_user
-
+  
   def index
   end
 
   def show
+    patient = find_patient
+    return unless patient
+    render json: {patient: patient.attributes.merge(
+      profile_pic: rails_blob_path(patient.profile_pic, only_path: true))}
   end
 
   def create
@@ -20,16 +24,29 @@ class V1::PatientsController < ApplicationController
   end
 
   def update
+    patient = find_patient
+    return unless patient
+
+    if patient.require(:patient).update(update_params)
+      render json: { patient: patient.attributes }, status: :accepted 
+    else 
+      render json: { message: 'Cannot update patient' }
+    end
   end
 
   def destroy
+    patient = find_patient
+    if patient&.destroy
+      render json: { status: :accepted, message: 'Deleted the patient', patient: patient.attributes }
+    else
+      render json: { message: 'Could not delete patient' }
+    end
   end
 
   private
 
   def patient_params
     params
-      .require(:patient)
       .permit(
         :given_name,
         :last_name,
@@ -38,5 +55,22 @@ class V1::PatientsController < ApplicationController
         :profile_pic,
         :relationship
       )
+  end
+
+  def find_patient
+    patient = pundit_user.patients.find_by(id: params[:id])
+    return patient if patient
+    render json: { error: "Can\'t find patient" }
+  end
+  
+  def update_params
+    params.permit(
+      :given_name,
+      :last_name,
+      :birthdate,
+      :gender,
+      :profile_pic,
+      :relationship
+    ).merge(registrant: pundit_user)
   end
 end
