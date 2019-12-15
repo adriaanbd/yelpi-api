bf4558335b742c3692ece223101d5811f725ca0b
Adriaan Beiertz <adriaanbd@gmail.com>
2019-12-15 18:12:34 -0500

    updating resource to resources patients for index action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     resources :sessions, only: [:create]
     resources :users, only: [:show, :create, :update, :destroy]
-    resource :patients, except: [:new, :edit]
+    resources :patients, except: [:new, :edit]
   end
 end
