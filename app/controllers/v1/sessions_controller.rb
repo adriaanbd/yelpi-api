bba263ff3bd1b70f59d594acef6e6d58b45aadaa
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-18 19:32:05 +0800

    Create user partial and session create jbuilders

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class V1::SessionsController < ApplicationController
 
     if user &.valid_password?(params[:password])
       token = JsonWebToken.encode({id: user.id})
-      render json: {user: user.attributes, token: token}, status: 201
+      render :create, locals: {user: user, token: token}, status: 201
     else
       render json: { error: "Invalid credentials"}, status: 401
     end
