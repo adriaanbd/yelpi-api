20ea21fc8100b67c833676841084f0ca65adae69
Adriaan Beiertz <adriaanbd@gmail.com>
2019-12-26 17:31:32 -0500

    added medication policy

---
+++ app/policies/medication_policy.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class MedicationPolicy < ApplicationPolicy
+  def show?
+    @user == @record.registrant
+  end
+
+  def update?
+    @user == @record.registrant
+  end
+
+  def destroy?
+    @user == @record.registrant
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
