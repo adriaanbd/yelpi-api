dd4731e4daa785e491633c33587c546414ea429d
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-28 12:55:26 +0800

    Add show action to observed_patients cont

--- app/controllers/v1/users/observed_patients_controller.rb
+++ app/controllers/v1/users/observed_patients_controller.rb
@@ -2,8 +2,25 @@ class V1::Users::ObservedPatientsController < ApplicationController
   before_action :pundit_user
   
   def index
-    observed_patients = pundit_user.observed_patients
+    patients = pundit_user.observed_patients
 
-    render :patients, locals: { patients: observed_patients }, status: :ok
+    render :patients, locals: { patients: patients }, status: :ok
+  end
+
+  def show
+    patient = find_patient
+    return unless patient
+
+    render :patient, locals: { patient: patient }, status: 200
+  end
+
+  private
+
+  def find_patient
+    patient = pundit_user.observed_patients.find_by(id: params[:id])
+    return patient if patient
+
+    find_error('patient')
+    nil
   end
 end
